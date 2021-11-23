6505eaac7f069e6428ee436ae71027e62d1c9de6
Cory Cherven <chervenc@my.canisius.edu>
2021-11-22 19:19:36 -0500

    Adds CA$H to page

--- web/src/sections/Team.tsx
+++ web/src/sections/Team.tsx
@@ -4,6 +4,7 @@ import { useStyletron } from 'styletron-react';
 import Isis from '../assets/Team/Isis Sakura.png';
 import Olly from '../assets/Team/Olly Bellwether.png';
 import Seth from '../assets/Team/Seth Horus.png';
+import Cash from '../assets/Team/CA$H.gif';
 import useMobile from '../hooks/useMobile';
 
 export const Team = (): JSX.Element => {
@@ -133,6 +134,38 @@ export const Team = (): JSX.Element => {
                                 })}
                             >
                                 Isis Sakura
+                            </h2>
+                        </div>
+                        <div
+                            className={css({
+                                position: 'relative',
+                                margin: isMobile ? '10px' : '20px',
+                                borderRadius: '20px',
+                                overflow: 'hidden',
+                                width: isMobile ? '100%' : undefined,
+                            })}
+                        >
+                            <img
+                                src={Cash}
+                                alt="CASH"
+                                className={css({
+                                    width: isMobile ? '100%' : '500px',
+                                    height: 'auto',
+                                })}
+                            />
+                            <h2
+                                className={css({
+                                    position: 'absolute',
+                                    bottom: '0px',
+                                    padding: '10px',
+                                    left: '0px',
+                                    textAlign: 'center',
+                                    right: '0px',
+                                    margin: 'auto',
+                                    backgroundColor: 'rgba(0, 0, 0, .4)',
+                                })}
+                            >
+                                CA$H
                             </h2>
                         </div>
                     </div>
